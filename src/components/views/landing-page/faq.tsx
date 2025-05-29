7d2f5e1d0d9ca7477c1de3aa3551b883da96621d
Santiago Sarabia <blvckcenturion@gmail.com>
2025-05-29 10:12:28 -0400

    fix: build issues

--- src/components/views/landing-page/faq.tsx
+++ src/components/views/landing-page/faq.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import {
-  ChevronDown,
-  ChevronUp,
-  HelpCircle,
-  Expand,
-  Minimize2,
-} from "lucide-react";
+import { ChevronDown, HelpCircle, Expand, Minimize2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 interface FAQItem {
